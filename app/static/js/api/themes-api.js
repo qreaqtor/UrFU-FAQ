130423713a7be40cdb40631375c71c02b42e3744
Горемыкин Алексей <goraleksey.jr@gmail.com>
2023-07-11 10:34:10 +0400

    теперь возвращает темы + асинхронные запросы

--- app/static/js/api/themes-api.js
+++ app/static/js/api/themes-api.js
@@ -1,7 +1,28 @@
-// нуписать функции получающие все темы, вопросы и ответы и возвращающие их в виде массивов
-const getApiThemes = () => {};
-const getApiQuestions = () => {};
-const getApiAnswers = () => {};
-
-
-export { getApiAnswers, getApiQuestions, getApiThemes };
+const getApiThemes = () => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', '/all_topics/');
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          const response = xhr.responseText;
+          resolve(response);
+        }
+      };
+      xhr.send();
+    });
+  };
+  
+  const getApiQuestions = () => {
+    return new Promise((resolve, reject) => {
+      // Ваш код для выполнения GET-запроса getApiQuestions()
+    });
+  };
+  
+  const getApiAnswers = () => {
+    return new Promise((resolve, reject) => {
+      // Ваш код для выполнения GET-запроса getApiAnswers()
+    });
+  };
+  
+  export { getApiAnswers, getApiQuestions, getApiThemes };
+  
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -3,26 +3,28 @@ import BoardPresenter from './presenter/board-presenter.js';
 import ThemeModel from './model/theme-model.js';
 
 import { getAnswers, getQuestions, getThemes } from './mock/themes.js';
-import { getApiAnswers, getApiQuestions, getApiThemes } from './api/themes-api.js'
+import { getApiAnswers, getApiQuestions, getApiThemes } from './api/themes-api.js';
 import { render } from './render.js';
-
 
 const headerElement = document.querySelector('.header');
 const mainElement = document.querySelector('.main');
-const boardPresenter = new BoardPresenter(mainElement.querySelector('.main__continer'))
-
-
-// заменить на getApi()
-const themes = getThemes();
-const questions = getQuestions();
-const answers = getAnswers();
+const boardPresenter = new BoardPresenter(mainElement.querySelector('.main__container'));
 
 const themeModel = new ThemeModel();
 const menu = new MenuView();
 
 render(menu, headerElement.querySelector('.header__container'));
 
+async function initialize() {
+    const themesResponse = await getApiThemes();
+    const themes = JSON.parse(themesResponse);
 
-menu.init(boardPresenter.switchToOtherPage);
-themeModel.init(themes, questions, answers);
-boardPresenter.init(themeModel);
+    const questions = getQuestions();
+    const answers = getAnswers();
+
+    themeModel.init(themes, questions, answers);
+    boardPresenter.init(themeModel);
+    menu.init(boardPresenter.switchToOtherPage);
+}
+
+initialize();
